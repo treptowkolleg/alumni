d7a5f1a0bdf61ed9a2fa2cf1ff63c8274f39bb99
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-12-24 09:33:59 +0100

    update

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/', name: 'app_')]
+#[Route('/', name: 'app_', methods: ['GET','POST'])]
 class AppController extends AbstractController
 {
+
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
+            $this->addFlash("success", "Vielen Dank für Deine Anmeldung!");
+            return $this->redirectToRoute('app_index');
+        }
+
         return $this->render('app/maintaining.html.twig', [
-            'controller_name' => 'AppController',
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Form/NewsletterType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Newsletter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsletterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'form-control form-control-lg', 'placeholder' => 'E-Mail-Adresse'],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary btn-lg'],
+                'label' => 'Newsletter anmelden'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Newsletter::class,
+        ]);
+    }
+}
