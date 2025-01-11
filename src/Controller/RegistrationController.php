bd800973eafbb1120c41facbc47f77405b031887
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-01-11 18:34:34 +0100

    update

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -70,7 +70,6 @@ class RegistrationController extends AbstractController
                 ->from(new Address('aulumniportal@treptowkolleg.de', 'Alumni Portal'))
                 ->to((string) $user->getEmail())
                 ->subject('Bitte bestätige deine Email-Adresse')
-                ->setHeaders()
                 ->htmlTemplate('registration/confirmation_email.html.twig')
                 ->context(['user' => $user])
         );
