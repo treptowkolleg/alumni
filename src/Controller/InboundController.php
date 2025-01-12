914df84927fc1a14734b6c76399e8c04bfae7436
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-01-12 09:01:08 +0100

    update

---
+++ src/Controller/InboundController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Inbound;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/inbound', name: 'inbound_', methods: ['GET','POST'])]
+class InboundController extends AbstractController
+{
+
+    #[Route('/', name: 'receive', methods: ['POST'])]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $inbound = new Inbound();
+        $inbound->setSender($request->get('from'));
+        $inbound->setSubject($request->get('subject'));
+        $inbound->setHtml($request->get('html'));
+        $inbound->setText($request->get('text'));
+        $entityManager->persist($inbound);
+        $entityManager->flush();
+        return new Response('OK', Response::HTTP_OK);
+    }
+
+}
---
+++ src/Entity/Inbound.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InboundRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+#[ORM\Entity(repositoryClass: InboundRepository::class)]
+class Inbound
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $sender = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $text = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $html = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $subject = null;
+
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: false)]
+    #[Gedmo\Timestampable(on: 'create')]
+    private ?\DateTimeImmutable $created = null;
+
+    public function __toString(): string
+    {
+        return $this->subject;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSender(): ?string
+    {
+        return $this->sender;
+    }
+
+    public function setSender(?string $sender): static
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getHtml(): ?string
+    {
+        return $this->html;
+    }
+
+    public function setHtml(?string $html): static
+    {
+        $this->html = $html;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeImmutable
+    {
+        return $this->created;
+    }
+
+}
