da5f4079111bcd1fcdcf0e1b501578a23c3fbbc0
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-01-13 07:18:21 +0100

    update

--- src/Enums/PerformanceCourseEnum.php
+++ src/Enums/PerformanceCourseEnum.php
@@ -4,22 +4,22 @@ namespace App\Enums;
 
 enum PerformanceCourseEnum: string
 {
-    case inf = "inf";
-    case ma = "ma";
-    case ch = "ch";
     case bi = "bi";
+    case ch = "ch";
+    case ds = "ds";
     case en = "en";
     case fr = "fr";
-    case sp = "sp";
-    case spo = "spo";
+    case inf = "inf";
+    case ku = "ku";
+    case la = "la";
+    case ma = "ma";
+    case mu = "mu";
     case ph = "ph";
     case phi = "phi";
-    case ku = "ku";
     case psy = "psy";
-    case ds = "ds";
-    case la = "la";
-    case mu = "mu";
     case pw = "pw";
+    case sp = "sp";
+    case spo = "spo";
 
 
 }
