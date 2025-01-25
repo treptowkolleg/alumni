c2bc0799505bc342af65aeee8e5278c2387349e2
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-01-25 23:25:31 +0100

    update

---
+++ src/Controller/Admin/EventCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Event;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class EventCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Event::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            FormField::addPanel("Veranstaltungsdetails"),
+            TextField::new('title'),
+            TextEditorField::new('description')->onlyOnForms(),
+            TextField::new('website')->onlyOnForms(),
+            TextField::new('location')->onlyOnForms(),
+        ];
+    }
+
+}
