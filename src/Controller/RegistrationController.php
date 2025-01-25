4752829820472b703e9038d53dfbb4d669a7d6bf
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-01-25 15:40:46 +0100

    solve bugs

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -92,7 +92,7 @@ class RegistrationController extends AbstractController
     #[Route('/verify/email', name: 'app_verify_email')]
     public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
     {
-        // $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
