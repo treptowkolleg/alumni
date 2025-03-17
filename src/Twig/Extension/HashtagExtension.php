20355b6d541ff7283225e714dd0b86e8d29cfae3
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-03-17 02:03:26 +0100

    update

--- src/Twig/Extension/HashtagExtension.php
+++ src/Twig/Extension/HashtagExtension.php
@@ -19,10 +19,13 @@ class HashtagExtension extends AbstractExtension
     public function hashtagFilter($text): array|string|null
     {
         // Regex für Hashtags
+        $decodedText = html_entity_decode($text);
+
+        // Regex für Hashtags, der auch Umlaute und andere Zeichen erfasst, aber Apostroph ausschließt
         return preg_replace_callback('/#([A-Za-z0-9ÄäÖöÜüß_-]+)/u', function($matches) {
             // Verlinkt den Hashtag zu einer Seite (z. B. /hashtag/{Hashtag})
-            return '<a href="#' . urlencode($matches[1]) . '">#' . htmlspecialchars($matches[1]) . '</a>';
-        }, $text);
+            return '<a href="/hashtag/' . urlencode($matches[1]) . '">#' . htmlspecialchars($matches[1]) . '</a>';
+        }, $decodedText);
     }
 
 }
