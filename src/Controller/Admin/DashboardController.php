faa624ee495c776272f45f72f144ddbf699087f7
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-03-20 10:17:58 +0100

    update

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -82,7 +82,7 @@ class DashboardController extends AbstractDashboardController
             'title' => 'test',
             'posts' => $posts,
             'news_articles' => $newsPosts,
-            'server_time' => date("Y-m-d H:i:s")
+            'server_time' => new \DateTimeImmutable("now")
         ]);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
